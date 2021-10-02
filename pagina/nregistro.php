d8bbaabc3b31a6236c71315cc73f20d372ff64d3
juan och <juanoch@gmail.com>
2021-10-01 19:10:32 -0600

    creacion de mi sistema de facturacion

--- pagina/nregistro.php
+++ pagina/nregistro.php
@@ -51,7 +51,7 @@ $consulta = mysqli_query($conn,"SELECT * FROM contacto WHERE fk_usuario ='".$fg.
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php?fg=<?php echo $fg; ?>">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
